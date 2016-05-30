1799d646dcbdbc2d56ab7b074c5e9a3b2f33fab1
Tim Hatch <tim@timhatch.com>
2016-05-30 10:27:19 -0700

    stlpy: Fix binary test file generation; check in products despite gitignore

--- stlpy/test_loader.py
+++ stlpy/test_loader.py
@@ -12,41 +12,41 @@ class StlTestHelpers(object):
 
 class TestLoader(unittest.TestCase):
     def test_cube_loader_ascii(self):
-        m = stl.Mesh('files/cube_ascii.stl')
+        m = stl.Mesh('files/cube-ascii.stl')
         self.assertEqual(len(m.facets), 12)
         self.assertEqual(len(m.vertex_cache), 8)
 
     def test_loader_ascii(self):
-        m = stl.Mesh('files/demo_ascii.stl')
+        m = stl.Mesh('files/demo-ascii.stl')
         self.assertEqual(len(m.facets), 1188)
 
 
 class TestRoundtrips(unittest.TestCase, StlTestHelpers):
     def test_cube_write(self):
-        m = stl.Mesh('files/cube_ascii.stl')
+        m = stl.Mesh('files/cube-ascii.stl')
         # TODO tempdir, ensure it doesn't exist, etc
         m.write_stl('temp.stl')
         m2 = stl.Mesh('temp.stl')
         self.assertComparesIdentical(m, m2)
 
     def test_cube_noop_scale(self):
-        m1 = stl.Mesh('files/cube_ascii.stl')
-        m2 = stl.Mesh('files/cube_ascii.stl')
+        m1 = stl.Mesh('files/cube-ascii.stl')
+        m2 = stl.Mesh('files/cube-ascii.stl')
         m2.scale((0.5,0.5,0.5))
         self.assertRaises(AssertionError, self.assertComparesIdentical, m1, m2)
         m2.scale((2.0, 2.0, 2.0))
         self.assertComparesIdentical(m1, m2)
-        
+
     def test_cube_noop_stretch(self):
-        m1 = stl.Mesh('files/cube_ascii.stl')
-        m2 = stl.Mesh('files/cube_ascii.stl')
+        m1 = stl.Mesh('files/cube-ascii.stl')
+        m2 = stl.Mesh('files/cube-ascii.stl')
         m2.stretch('x', 3, 5, 1)
         self.assertComparesIdentical(m1, m2)
 
 class StretchTest(unittest.TestCase, StlTestHelpers):
     def test_cube_stretched_inside(self):
-        m1 = stl.Mesh('files/cube_ascii.stl')
-        m2 = stl.Mesh('files/cube_ascii.stl')
+        m1 = stl.Mesh('files/cube-ascii.stl')
+        m2 = stl.Mesh('files/cube-ascii.stl')
         m1.scale((1.2, 1.0, 1.0))
         m2.stretch('x', 0.1, 0.9, 0.2)
         self.assertComparesIdentical(m1, m2)
